ba2c497663242af12ee5e5b31caa543b72af5800
Erik Skjenna <ttv.pumpkinfuture@gmail.com>
2026-06-24 17:45:50 -0400

    RED: add equality tests for Tic board - JUnit: 3 tests, 2 failures

--- Lab4/tic_java_sample_code/TicTest.java
+++ Lab4/tic_java_sample_code/TicTest.java
@@ -6,12 +6,34 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 
-	@Test
-	void test() {
-		Tic board = new Tic(3, 3);
-		Tic board2 = new Tic(3,3);
-		assertEquals(board, board2);
-		//testing ability to declare a board
-	}
-	
+    @Test
+    void constructorInitializesEmptyBoard() {
+        Tic board = new Tic(3, 3);
+
+        assertEquals(3, board.rows);
+        assertEquals(3, board.cols);
+        assertEquals("X", board.turn);
+
+        for (int i = 0; i < board.rows; i++) {
+            for (int j = 0; j < board.cols; j++) {
+                assertEquals("_", board.board[i][j]);
+            }
+        }
+    }
+
+    @Test
+    void twoNewBoardsWithSameSizeShouldBeEqual() {
+        Tic board = new Tic(3, 3);
+        Tic board2 = new Tic(3, 3);
+
+        assertEquals(board, board2);
+    }
+
+    @Test
+    void equalBoardsShouldHaveSameHashCode() {
+        Tic board = new Tic(3, 3);
+        Tic board2 = new Tic(3, 3);
+
+        assertEquals(board.hashCode(), board2.hashCode());
+    }
 }
