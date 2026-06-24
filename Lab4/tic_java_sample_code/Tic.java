7858e6cf0d844e7c61efaa3477d43075cc8ad403
Erik Skjenna <ttv.pumpkinfuture@gmail.com>
2026-06-24 17:46:17 -0400

    GREEN: implement equals and hashCode for Tic - JUnit: 3 tests passing

--- Lab4/tic_java_sample_code/Tic.java
+++ Lab4/tic_java_sample_code/Tic.java
@@ -1,19 +1,49 @@
 package tic;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Tic {
-	String[][] board;
-	int rows;
-	int cols;
-	String turn;
-	public Tic(int row, int col) {
-		board = new String[row][col];
-		rows = row;
-		cols = col;
-		turn = "X";
-		for(int i = 0; i<row; i++) {
-			for(int j = 0; j<col; j++) {
-				board[i][j] = "_";
-			}
-		}
-	}
+    String[][] board;
+    int rows;
+    int cols;
+    String turn;
+
+    public Tic(int row, int col) {
+        board = new String[row][col];
+        rows = row;
+        cols = col;
+        turn = "X";
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                board[i][j] = "_";
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Tic)) {
+            return false;
+        }
+
+        Tic other = (Tic) obj;
+
+        return rows == other.rows
+                && cols == other.cols
+                && Objects.equals(turn, other.turn)
+                && Arrays.deepEquals(board, other.board);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(rows, cols, turn);
+        result = 31 * result + Arrays.deepHashCode(board);
+        return result;
+    }
 }
